04d8cd72b86f021ee28e89ae85765bf5ca124483
abhimanyu <abhimanyu.singh@kuliza.com>
2018-09-13 15:10:42 +0530

    Further changes

--- Assignment/js/data.js
+++ Assignment/js/data.js
@@ -1,110 +1,120 @@
 'use strict';
 
 var placeholderImage = './assets/placeholder.png';
-var imageConfig = [
-    {
-        id: 0,
-        placeholder: placeholderImage,
-        src: './assets/one.jpeg',
-    },
-    {
-        id: 1,
-        placeholder: placeholderImage,
-        src: './assets/two.jpeg',
-    },
-    {
-        id: 2,
-        placeholder: placeholderImage,
-        src: './assets/three.jpeg',
-    },
-    {
-        id: 3,
-        placeholder: placeholderImage,
-        src: './assets/four.jpeg',
-    },
-    {
-        id: 4,
-        placeholder: placeholderImage,
-        src: './assets/five.jpeg',
-    },
-    {
-        id: 5,
-        placeholder: placeholderImage,
-        src: './assets/six.jpeg',
-    },
-    {
-        id: 6,
-        placeholder: placeholderImage,
-        src: './assets/seven.jpeg',
-    },
-    {
-        id: 7,
-        placeholder: placeholderImage,
-        src: './assets/eight.jpeg',
-    },
-    {
-        id: 8,
-        placeholder: placeholderImage,
-        src: './assets/nine.jpeg',
-    },
-    {
-        id: 9,
-        placeholder: placeholderImage,
-        src: './assets/ten.jpeg',
-    },
-    {
-        id: 10,
-        placeholder: placeholderImage,
-        src: './assets/eleven.jpeg',
-    },
-    {
-        id: 11,
-        placeholder: placeholderImage,
-        src: './assets/twelve.jpeg',
-    },
-    {
-        id: 12,
-        placeholder: placeholderImage,
-        src: './assets/thirteen.jpeg',
-    },
-    {
-        id: 13,
-        placeholder: placeholderImage,
-        src: './assets/fourteen.jpeg',
-    },
-    {
-        id: 14,
-        placeholder: placeholderImage,
-        src: './assets/fifteen.jpeg',
-    },
-    {
-        id: 15,
-        placeholder: placeholderImage,
-        src: './assets/sixteen.jpeg',
-    },
-    {
-        id: 16,
-        placeholder: placeholderImage,
-        src: './assets/seventeen.jpeg',
-    },
-    {
-        id: 17,
-        placeholder: placeholderImage,
-        src: './assets/eighteen.jpeg',
-    },
-    {
-        id: 18,
-        placeholder: placeholderImage,
-        src: './assets/nineteen.jpeg',
-    },
-    {
-        id: 19,
-        placeholder: placeholderImage,
-        src: './assets/twenty.jpeg',
-    },
-    {
-        id: 20,
-        placeholder: placeholderImage,
-        src: './assets/six.jpeg',
-    },
-];
+var deleteIcon = './assets/rubbish-bin.svg';
+var imageConfig = [{
+    id: 0,
+    placeholder: placeholderImage,
+    src: './assets/one.jpeg',
+    imageDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+}, {
+    id: 1,
+    placeholder: placeholderImage,
+    src: './assets/two.jpeg',
+    imageDescription: 'Ut tincidunt ligula vitae arcu consequat euismod',
+}, {
+    id: 2,
+    placeholder: placeholderImage,
+    src: './assets/three.jpeg',
+    imageDescription: 'Donec dignissim quam at massa suscipit pellentesque.',
+}, {
+    id: 3,
+    placeholder: placeholderImage,
+    src: './assets/four.jpeg',
+    imageDescription: 'Pellentesque ac rhoncus justo.',
+}, {
+    id: 4,
+    placeholder: placeholderImage,
+    src: './assets/five.jpeg',
+    imageDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+}, {
+    id: 5,
+    placeholder: placeholderImage,
+    src: './assets/six.jpeg',
+    imageDescription: 'Donec dignissim quam at massa suscipit pellentesque.',
+}, {
+    id: 6,
+    placeholder: placeholderImage,
+    src: './assets/seven.jpeg',
+    imageDescription: 'Ut tincidunt ligula vitae arcu consequat euismod',
+}, {
+    id: 7,
+    placeholder: placeholderImage,
+    src: './assets/eight.jpeg',
+    imageDescription: 'Donec dignissim quam at massa suscipit pellentesque.',
+}, {
+    id: 8,
+    placeholder: placeholderImage,
+    src: './assets/nine.jpeg',
+    imageDescription: 'Pellentesque ac rhoncus justo.',
+}, {
+    id: 9,
+    placeholder: placeholderImage,
+    src: './assets/ten.jpeg',
+    imageDescription: 'Ut tincidunt ligula vitae arcu consequat euismod',
+}, {
+    id: 10,
+    placeholder: placeholderImage,
+    src: './assets/eleven.jpeg',
+    imageDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+}, {
+    id: 11,
+    placeholder: placeholderImage,
+    src: './assets/twelve.jpeg',
+    imageDescription: 'Donec dignissim quam at massa suscipit pellentesque.',
+}, {
+    id: 12,
+    placeholder: placeholderImage,
+    src: './assets/thirteen.jpeg',
+    imageDescription: 'Pellentesque ac rhoncus justo.',
+}, {
+    id: 13,
+    placeholder: placeholderImage,
+    src: './assets/fourteen.jpeg',
+    imageDescription: 'Donec dignissim quam at massa suscipit pellentesque.',
+}, {
+    id: 14,
+    placeholder: placeholderImage,
+    src: './assets/fifteen.jpeg',
+    imageDescription: 'Ut tincidunt ligula vitae arcu consequat euismod',
+}, {
+    id: 15,
+    placeholder: placeholderImage,
+    src: './assets/sixteen.jpeg',
+    imageDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+}, {
+    id: 16,
+    placeholder: placeholderImage,
+    src: './assets/seventeen.jpeg',
+    imageDescription: 'Ut tincidunt ligula vitae arcu consequat euismod',
+}, {
+    id: 17,
+    placeholder: placeholderImage,
+    src: './assets/eighteen.jpeg',
+    imageDescription: 'Pellentesque ac rhoncus justo.',
+}, {
+    id: 18,
+    placeholder: placeholderImage,
+    src: './assets/nineteen.jpeg',
+    imageDescription: 'Donec dignissim quam at massa suscipit pellentesque.',
+}, {
+    id: 19,
+    placeholder: placeholderImage,
+    src: './assets/twenty.jpeg',
+    imageDescription: 'Ut tincidunt ligula vitae arcu consequat euismod',
+}, {
+    id: 20,
+    placeholder: placeholderImage,
+    src: './assets/six.jpeg',
+    imageDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+}, {
+    id: 21,
+    placeholder: placeholderImage,
+    src: './assets/four.jpeg',
+    imageDescription: 'Ut tincidunt ligula vitae arcu consequat euismod',
+}, {
+    id: 22,
+    placeholder: placeholderImage,
+    src: './assets/seven.jpeg',
+    imageDescription: 'Donec dignissim quam at massa suscipit pellentesque.',
+}, ];
--- Assignment/js/index.js
+++ Assignment/js/index.js
@@ -1,21 +1,26 @@
 'use strict';
 
 var gridLayout = document.getElementById('gridLayout');
-var deleteIcon = './assets/rubbish-bin.svg';
+var shuffleImages = document.getElementById('shuffleImages');
 
+/* Add shuffle method to array proto */
 Array.prototype.shuffle = function() {
     var input = this;
-    for (var i = input.length - 1 ; i >= 0 ; i--) {
-        var randomIndex = Math.floor(Math.random()*(i+1)); 
-        var itemAtIndex = input[randomIndex]; 
-        input[randomIndex] = input[i]; 
+    for (var i = input.length - 1; i >= 0; i--) {
+        var randomIndex = Math.floor(Math.random() * (i + 1));
+        var itemAtIndex = input[randomIndex];
+        input[randomIndex] = input[i];
         input[i] = itemAtIndex;
     }
     return input;
 }
 
-function generateImages() {
-    for (var i = 0 ; i < imageConfig.length ; i ++) {
+/* Function for generating images based on config */
+function generateImages(updateImage) {
+    for (var i = 0; i < imageConfig.length; i++) {
+        var image = placeholderImage;
+        if (!updateImage)
+            image = imageConfig[i].src;
         var elementWrapper = document.createElement('div');
         var imageFlipper = document.createElement('div');
         var imageSection = document.createElement('div');
@@ -23,32 +28,56 @@ function generateImages() {
         var imageDescription = document.createElement('div');
         var deleteImage = document.createElement('button');
         elementWrapper.setAttribute('class', 'image-wrapper-section');
+        elementWrapper.setAttribute('id', 'unique_' + imageConfig[i].id);
         imageFlipper.setAttribute('class', 'image-flipper');
         imageSection.setAttribute('class', 'image-section');
-        imageTag.setAttribute('src', imageConfig[i].src);
-        imageTag.setAttribute('id', imageConfig[i].id);
+        imageTag.setAttribute('src', image);
         imageDescription.setAttribute('class', 'image-description');
-        imageDescription.innerHTML = 'This is flipped text';
-        deleteImage.innerHTML = "Delete <img src="+ deleteIcon +" />";
+        imageDescription.innerHTML = '<span>' + imageConfig[i].imageDescription + '</span>';
+        imageDescription.appendChild(deleteImage);
+        deleteImage.innerHTML = 'Delete';
         deleteImage.setAttribute('class', 'delete-button');
+        deleteImage.setAttribute('id', imageConfig[i].id);
         imageSection.appendChild(imageTag);
         imageSection.appendChild(imageDescription);
         imageFlipper.appendChild(imageSection);
         elementWrapper.appendChild(imageFlipper);
-        elementWrapper.appendChild(deleteImage);
         gridLayout.appendChild(elementWrapper);
     }
 }
 
+/* Function to replace images on window load event */
+function replaceImages() {
+    for (var i = 0; i < imageConfig.length; i++) {
+        document.querySelectorAll('.image-section img')[i].src = imageConfig[i].src;
+    }
+}
+
+/* Function to shuffle array randomly */
 function shuffleData() {
     imageConfig.shuffle();
     gridLayout.innerHTML = '';
-    generateImages();
+    generateImages(false);
 }
 
-/* Event Delegation */
+/* Delete Event Delegation */
 gridLayout.addEventListener('click', function(event) {
-    event.target.parentNode.outerHTML = '';
+    event.stopImmediatePropagation();
+    var removeId = 'unique_' + event.target.id;
+    if (document.getElementById(removeId) !== null)
+        document.getElementById(removeId).outerHTML = '';
 });
 
-generateImages();
+shuffleImages.addEventListener('click', function(event) {
+    shuffleData();
+});
+
+/* Initialize the app */
+(function() {
+    generateImages(true);
+    window.onload = function() {
+        setTimeout(function() {
+            replaceImages();
+        }, 800)
+    }
+})();
